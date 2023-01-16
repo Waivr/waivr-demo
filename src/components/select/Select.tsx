cd94f245a486d1dbde30ee3cd6e986f1a582ac52
watsoncolin <colin@colinwatson.me>
2023-01-16 15:54:30 -0600

    styles select

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,1 +1,46 @@
-export const Select = (props: any) => <>subscription selection</>;
+import { Box } from '@mui/system';
+
+type Option = {
+  label: string;
+  value: string;
+  selected?: boolean;
+};
+
+type Props = {
+  options: Array<Option>;
+  name: string;
+};
+
+const styles = {
+  input: { appearance: 'none' },
+  label: {
+    cursor: 'pointer',
+    fontSize: '12px',
+    lineHeight: '15px',
+    color: '#969B9F',
+    borderBottomColor: '#D9D9D9',
+    borderBottomWdith: '2px',
+    borderBottomStyle: 'solid',
+    '&.selected': {
+      color: '#172836',
+      borderBottomColor: '#172836',
+    },
+  },
+  div: {
+    display: 'inline-block',
+    ':not(:first-child)': {
+      marginLeft: '40px',
+    },
+  },
+};
+
+export const Select = ({ name, options = [] }: Props) => (
+  <Box sx={{ ...styles }}>
+    {options.map((o: Option) => (
+      <Box>
+        <label className={o.selected ? 'selected' : ''}>{o.label}</label>
+        <input type="radio" name={name} value={o.value} checked={o.selected} />
+      </Box>
+    ))}
+  </Box>
+);
