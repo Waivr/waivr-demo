c32f107b90a6dc003247656b10a5c9387db02697
watsoncolin <colin@colinwatson.me>
2023-01-18 15:54:03 -0700

    adds logo

--- src/components/logo/Logo.stories.tsx
+++ src/components/logo/Logo.stories.tsx
@@ -1,15 +1,18 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Logo } from './logo';
+import { Logo } from './Logo';
 
 export default {
   title: 'Components/Logo',
   component: Logo,
   argTypes: {
+    url: { name: 'Url' },
   },
 } as ComponentMeta<typeof Logo>;
 
-const Template: ComponentStory<typeof Logo> = (args) => <Logo />;
+const Template: ComponentStory<typeof Logo> = (args) => <Logo {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  url: 'https://www.waivr.co/',
+};
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,1 +1,11 @@
-export const Logo = () => <>logo</>;
+import WaivrLogo from './logo.svg';
+
+type Props = {
+  url: string;
+};
+
+export const Logo = ({ url }: Props) => (
+  <a href={url}>
+    <img src={WaivrLogo} alt="Waivr" />
+  </a>
+);
