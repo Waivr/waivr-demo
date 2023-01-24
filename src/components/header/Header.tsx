6bafeb3c87aae71a8860401922807ab9c36e564c
watsoncolin <colin@colinwatson.me>
2023-01-23 20:05:24 -0600

    add docs link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/system';
+import AppTheme from '../common/global/AppTheme';
 import { Logo } from '../logo/Logo';
 
 type Props = {
@@ -12,6 +13,17 @@ const styles = {
   alignItems: 'center',
   header: {
     marginLeft: '45px',
+    width: '100%',
+  },
+  link: {
+    textAlign: 'right',
+    width: '100%',
+    a: {
+      color: AppTheme.palette.text.primary,
+      ':hover': {
+        color: '#232AE2',
+      },
+    },
   },
 };
 
@@ -19,5 +31,10 @@ export const Header = ({ label, url }: Props) => (
   <Box sx={{ ...styles }}>
     <Logo url={url} />
     <Box sx={{ ...styles.header }}>{label}</Box>
+    <Box sx={{ ...styles.link }}>
+      <a href="https://www.waivr.co/docs" target="docs">
+        API Docs
+      </a>
+    </Box>
   </Box>
 );
