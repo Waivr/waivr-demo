602a71a438a00ec12a9315b75c99ecc9119198f5
eduardowaivr <106112709+eduardowaivr@users.noreply.github.com>
2023-01-25 14:02:26 -0500

    WAIVR-146 - Stage Bypass (#23)
    - Added bypass config to be used against stage
    - Added how to use on onboardCustomer test ts

--- src/core/domain/merchant/merchantIdentifier.ts
+++ src/core/domain/merchant/merchantIdentifier.ts
@@ -1,13 +1,9 @@
 import RequiredAttributes from '../../paramutils/requiredAttributes';
 
 export class MerchantIdentifier {
-
   value: string;
 
   constructor(value: string) {
-
     this.value = RequiredAttributes.requireNonBlank(value);
-
   }
-
 }
--- test/integration/onboardCustomer.test.ts
+++ test/integration/onboardCustomer.test.ts
@@ -2,13 +2,23 @@ import EnvApiScopeEnd2End from './environment/envApiScope';
 import { CustomerCreateArgs } from '../../src/core/domain/customer/customerCreateArgs';
 import { Email } from '../../src/core/domain/common/contact/email';
 import { PersonName } from '../../src/core/domain/person/personName';
+import { MerchantIdentifier } from '../../src/core/domain/merchant/merchantIdentifier';
+import { ConnectAccountCreateArgs } from '../../src/core/domain/connectaccount/connectAccountCreateArgs';
+import { ConnectAccountRenderCreateArgs } from '../../src/core/domain/connectaccount/connectAccountRenderCreateArgs';
+import {
+    ConnectAccountInstitutionCreateArgs
+} from '../../src/core/domain/connectaccount/connectAccountInstitutionCreateArgs';
+import { ConnectAccountPublicAccessToken } from '../../src/core/domain/connectaccount/connectAccountPublicAccessToken';
 
 describe.skip('Onboard Customer End2End', () => {
     const envApiRegistries = EnvApiScopeEnd2End.stage();
     const { waivrAppApiRegistry, merchantIdentifier, apiToken } = envApiRegistries;
 
-    it.skip('onboardCustomer ', async () => {
+    it.skip('onboardCustomer using a ByPass Bank connected via PLAID ', async () => {
+        // Creates a new customer
         const customerService = waivrAppApiRegistry.customerService();
+        const connectAccountService = waivrAppApiRegistry.connectAccountService();
+
         const customerCreateArgs = new CustomerCreateArgs(
             merchantIdentifier,
             new Email('johnsnow@northwall.com'),
@@ -16,7 +26,52 @@ describe.skip('Onboard Customer End2End', () => {
             null,
             null
         );
-        const loginCreationScope = await customerService.create(customerCreateArgs, apiToken);
-        expect(loginCreationScope).not.toBeNull();
+        const customer = await customerService.create(customerCreateArgs, apiToken);
+        expect(customer).not.toBeNull();
+
+        // Connects the bank account selected from as a Bypass with a Customer bank account at Waivr
+        const connectAccountCreateArgs = ConnectAccountCreateArgs.asBypass(
+            merchantIdentifier,
+            customer.identifier,
+        );
+        await connectAccountService.linkCustomerAccount(connectAccountCreateArgs, apiToken);
+    });
+
+    it.skip('onboardCustomer using a Bank connected via PLAID ', async () => {
+        // Creates a new customer
+        const customerService = waivrAppApiRegistry.customerService();
+        const connectAccountService = waivrAppApiRegistry.connectAccountService();
+
+        const customerCreateArgs = new CustomerCreateArgs(
+            merchantIdentifier,
+            new Email('johnsnow@northwall.com'),
+            PersonName.of('John', 'Snow'),
+            null,
+            null
+        );
+        const customer = await customerService.create(customerCreateArgs, apiToken);
+        expect(customer).not.toBeNull();
+
+        // Request a new rendering url from PLAID
+        const connectAccountRenderCreateArgs = new ConnectAccountRenderCreateArgs(
+            merchantIdentifier
+        );
+        const connectAccountRender = await connectAccountService.createRenderLink(connectAccountRenderCreateArgs, apiToken);
+        expect(connectAccountRender).not.toBeNull();
+
+        // Connects the bank account selected from as a Bypass with a Customer bank account at Waivr
+        const institution = new ConnectAccountInstitutionCreateArgs(
+            '<Bank Account Id from Plaid Response>'
+        );
+        const publicToken = new ConnectAccountPublicAccessToken(
+            '<Public Token provided by PLAID to exchange for Access Token>'
+        );
+        const connectAccountCreateArgs = new ConnectAccountCreateArgs(
+            merchantIdentifier,
+            customer.identifier,
+            institution,
+            publicToken
+        );
+        await connectAccountService.linkCustomerAccount(connectAccountCreateArgs, apiToken);
     });
 });
